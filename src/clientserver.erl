3b21ff6cdb0d3bff347ebac73cd3f154f0f421ca
dbenson24 <derekbenson24@gmail.com>
2017-11-29 21:44:26 -0500

    added helper functions for join a room and sending messages

--- src/clientserver.erl
+++ src/clientserver.erl
@@ -4,7 +4,7 @@
 
 -export([start_link/2, stop/1]).
 -export([init/1, handle_call/3, handle_cast/2, terminate/2]).
-
+-export([join_room/1, send_message/2]).
 
 
 % Client Functions
@@ -14,19 +14,23 @@ start_link(UserServer, UserName) ->
 stop(UserName) ->
     gen_server:stop(UserName).
 
+join_room(Room) ->
+    gen_server:call({clientserver, node()}, {join_room, list_to_atom(Room)}).
+
+send_message(Room, Message) ->
+    gen_server:call({clientserver, node()}, {send_message, list_to_atom(Room), Message}).
+
 
 init({UserServer, UserName}) ->
     ok = userserver:login(UserServer, UserName),
     io:format("clientserver: logged in to ~p as ~p~n", [UserServer, UserName]),
     {ok, {UserServer, UserName, #{}, []}}.
 
-handle_call({send_message, Message, Room}, _From, {UserServer, UserName, Rooms, MessageHandlers}) ->
-    RoomAtom = list_to_atom(Room),
+handle_call({send_message, RoomAtom, Message}, _From, {UserServer, UserName, Rooms, MessageHandlers}) ->
     #{RoomAtom := Node} = Rooms,
     {reply, chat:message({RoomAtom, Node}, Message), {UserServer, UserName, Rooms, MessageHandlers}};
 
-handle_call({join_room, Room}, _From, {UserServer, UserName, Rooms, MessageHandlers}) ->
-    RoomAtom = list_to_atom(Room),
+handle_call({join_room, RoomAtom}, _From, {UserServer, UserName, Rooms, MessageHandlers}) ->
     {ok, Node} = userserver:join_room(UserServer, RoomAtom, {clientserver, node()}),
     io:format("clientserver: joined room: ~p on ~p~n", [RoomAtom, Node]),
     {reply, ok, {UserServer, UserName, Rooms#{RoomAtom => Node}, MessageHandlers}}.
